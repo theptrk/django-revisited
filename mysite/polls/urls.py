187126be1f763dc8dae358228462c9e532ed28ad
Patrick <patrick.tran06@gmail.com>
2017-10-01 19:33:33 -0700

    add view and url and templates using django template engine with context

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+# use the app_name to namespace a url
+# so instead of  {% url 'detail' %} in the template we would now use
+# {% url polls:detail %}
+app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 def index(request):
     latest_questions_list = Question.objects.order_by('pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_questions_list])
-    return HttpResponse(output)
+    context = {
+        'latest_questions_list': latest_questions_list
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("Youre looking at question %s" % question_id)
+    # original implementation
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question': question})
+
+    # implementation with the shortcut "get_object_or_404"
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+    # there is also a "get_list_or_404()" function
 
 def results(request, question_id):
     response = "youre looking at the results of question %s"
