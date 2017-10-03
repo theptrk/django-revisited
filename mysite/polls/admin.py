3949049d73bbdc1b5f6bfbddfc9b87f47b1e1c50
Patrick <patrick.tran06@gmail.com>
2017-10-02 18:39:40 -0700

    add static files, styles, image of taipei

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # https://docs.djangoproject.com/en/1.11/intro/tutorial07/
+    search_fields = ['question_text']
+    list_filter = ['pub_date']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None,          {
+            'fields': ['question_text']
+        }),
+        ('Date Information', {
+            'fields': ['pub_date'],
+            'classes': ['collapse']
+        })
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
